989f8eecde74f75605af9a79e1b9e1440f6da54b
Evan Pfeifer <evanpfeifer@berkeley.edu>
2020-12-26 01:21:52 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Evan Pfeifer on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
